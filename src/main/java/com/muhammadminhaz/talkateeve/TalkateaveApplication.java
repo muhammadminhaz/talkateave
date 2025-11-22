880a6658980e402d5a6a91eaab06d5fac01af0c6
Minhaz <mminhazurrahman0@gmail.com>
2025-11-22 20:15:27 +0600

    rename

--- src/main/java/com/muhammadminhaz/talkateeve/TalkateaveApplication.java
+++ src/main/java/com/muhammadminhaz/talkateeve/TalkateaveApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TalkateeveApplication {
+public class TalkateaveApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(TalkateeveApplication.class, args);
+        SpringApplication.run(TalkateaveApplication.class, args);
     }
 
 }
--- src/test/java/com/muhammadminhaz/talkateeve/TalkateaveApplicationTests.java
+++ src/test/java/com/muhammadminhaz/talkateeve/TalkateaveApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class TalkateeveApplicationTests {
+class TalkateaveApplicationTests {
 
     @Test
     void contextLoads() {
