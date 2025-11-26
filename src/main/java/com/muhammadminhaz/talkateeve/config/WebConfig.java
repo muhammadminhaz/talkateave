fb1a518bc3c96bb3ded4a10ea01d8538881ac73c
Minhaz <mminhazurrahman0@gmail.com>
2025-11-26 18:17:33 +0600

    update link

--- src/main/java/com/muhammadminhaz/talkateeve/config/WebConfig.java
+++ src/main/java/com/muhammadminhaz/talkateeve/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000", "http://localhost:63342", "https://talkateave-fe.vercel.app")
+                        .allowedOrigins("http://localhost:3000", "http://localhost:63342", "https://talkateave.vercel.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
